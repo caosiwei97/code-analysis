03673998db2315c5ab3380a1ce485e175516d12c
caosiwei <caosiwei97@163.com>
2023-05-22 07:59:52 +0800

    feat:链式调用分析

--- test/bar.ts
+++ test/bar.ts
@@ -1,3 +1,13 @@
-import test from 'mod'
+import test, { name, age as Age } from 'mod'
+import test2 from 'mod2'
+import test3 from 'mod2'
 
 const bar: number = 1
+
+function testFn() {
+  const test = 1
+}
+
+testFn()
+
+test.testFn()
